ab2eb8b3e05567629305a84bde3e35d3ed9e506c
Elvin <84231975+ElvinEsedullayev@users.noreply.github.com>
2022-03-10 21:08:16 +0400

    admin auth sistemi ve middleware islemleri hazir

--- app/Http/Controllers/Back/AuthController.php
+++ app/Http/Controllers/Back/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
 
     public function logout()
     {
-        Auth::logout();
+        Auth::guard('admin')->logout();
         return redirect()->route('admin.login');
     }
 }
